155f5082ed09246f50396e84e5ee92ab9d43b693
Pavelz30 <pavel.zavodyan@mail.ru>
2024-05-21 22:47:49 +0300

    add my name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 	      <h1>Test of revert</h1>
         <h2>
-          Akilin Alexander
+          Zavodyan Pavel
         </h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
